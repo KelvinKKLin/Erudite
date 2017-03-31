ea580838dfff494c9372c42d442ab8dc429adc22
francium <varunhooda1@gmail.com>
2017-03-31 13:20:46 -0400

    (#1) Added server authentication for login.
    - Authentication token also stored in shared preferences

--- app/src/main/java/ca/mcmaster/plan6/erudite/LoginActivity.java
+++ app/src/main/java/ca/mcmaster/plan6/erudite/LoginActivity.java
@@ -1,9 +1,12 @@
 package ca.mcmaster.plan6.erudite;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.v4.content.res.ResourcesCompat;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -12,6 +15,11 @@ import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import ca.mcmaster.plan6.erudite.fetch.FetchAPIData;
 
 public class LoginActivity extends Activity {
 
@@ -52,11 +60,64 @@ public class LoginActivity extends Activity {
         mProgressView = findViewById(R.id.login_progress);
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mEmailView.setText("test@test.com");
+        mPasswordView.setText("password");
+    }
+
     private void attemptLogin() {
+        String urlLogin = "http://erudite.ml/login";
+
+        String email = mEmailView.getText().toString();
+        String password = mPasswordView.getText().toString();
+
+        JSONObject data = null;
+        try {
+            data = new JSONObject()
+                    .put("email", email)
+                    .put("password", password);
+        } catch (JSONException je) {
+            je.printStackTrace();
+            return;
+        }
+
+        new FetchAPIData() {
+            @Override
+            protected void onFetch(JSONObject data) {
+                try {
+                    if ((boolean) data.get("success")) {
+                        login((String) data.get("token"));
+                    } else {
+                        loginFailed();
+                    }
+                } catch (JSONException je) {
+                    je.printStackTrace();
+                }
+            }
+        }.fetch(urlLogin, data);
+    }
+
+    private void login(String token) {
+        storeToken(token);
+
         Intent result = new Intent();
         setResult(RESULT_OK, result);
         finish();
     }
 
-}
+    private void loginFailed() {
+        mEmailView.setTextColor(ResourcesCompat.getColor(getResources(), R.color.colorDanger, null));
+        mPasswordView.setTextColor(ResourcesCompat.getColor(getResources(), R.color.colorDanger, null));
+    }
 
+    private void storeToken(String token) {
+        SharedPreferences shared_pref = this.getSharedPreferences(
+                getString(R.string.pref_file_key), Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = shared_pref.edit();
+        editor.putString(getString(R.string.pref_key_token), token);
+        editor.commit();
+    }
+
+}
--- app/src/main/java/ca/mcmaster/plan6/erudite/MainActivity.java
+++ app/src/main/java/ca/mcmaster/plan6/erudite/MainActivity.java
@@ -45,7 +45,8 @@ public class MainActivity extends Activity {
             }
         });
 
-        launchSplashActivity();
+        //launchSplashActivity();   // Disabled for debugging during development.
+        launchLoginActivity();
     }
 
     @Override
