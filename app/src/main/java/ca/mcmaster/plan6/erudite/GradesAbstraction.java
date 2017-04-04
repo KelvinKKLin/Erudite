4956415967d199109147e6907606eb5f442fea27
puruj <puru123456789@hotmail.com>
2017-04-03 20:22:07 -0400

    Started parser for grades

---
+++ app/src/main/java/ca/mcmaster/plan6/erudite/GradesAbstraction.java
@@ -0,0 +1,32 @@
+package ca.mcmaster.plan6.erudite;
+
+/**
+ * Created by Puru on 2017-04-03.
+ */
+
+public class GradesAbstraction {
+
+
+    private String rawData;
+
+    private String success;
+    private String message;
+    private String userID;
+    private String email;
+    private String password;
+    private String account_type;
+    private String courses;
+
+    public GradesAbstraction(String rawData){
+        this.rawData = rawData;
+    }
+
+    public GradesAbstraction(){
+        this.rawData = "";
+    }
+
+    private void extractData(){
+
+    }
+
+}
