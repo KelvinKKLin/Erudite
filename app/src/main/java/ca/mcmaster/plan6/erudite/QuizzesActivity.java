bc7d2c0911d8810b01fff344409d3669b7127d3d
Kelvin Ka-Kwok Lin <linkk4@mcmaster.ca>
2017-04-04 22:19:40 -0400

    Added disable quiz feature after submission

--- app/src/main/java/ca/mcmaster/plan6/erudite/QuizzesActivity.java
+++ app/src/main/java/ca/mcmaster/plan6/erudite/QuizzesActivity.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -73,7 +74,14 @@ public class QuizzesActivity extends Activity {
                     submitButton.setOnClickListener(new View.OnClickListener() {
                         @Override
                         public void onClick(View v) {
-                            submitButtonPressed(studentAnswers, answers);
+                            boolean success = false;
+                            success = submitButtonPressed(studentAnswers, answers);
+                            if(success){
+                                answer1.setEnabled(false);
+                                answer2.setEnabled(false);
+                                answer3.setEnabled(false);
+                                submitButton.setEnabled(false);
+                            }
                         }
                     });
                 }
@@ -83,7 +91,7 @@ public class QuizzesActivity extends Activity {
         }
     }
 
-    private void submitButtonPressed(ArrayList<EditText> studentAnswers, ArrayList<String> answers){
+    private boolean submitButtonPressed(ArrayList<EditText> studentAnswers, ArrayList<String> answers){
         String[] studentAnswersText = extractStudentAnswers(studentAnswers);
         int numberCorrect = 0;
         for(int i = 0; i < studentAnswersText.length; i++){
@@ -92,10 +100,10 @@ public class QuizzesActivity extends Activity {
             }
         }
 
-        submitGrade(((double)numberCorrect/studentAnswersText.length)*100);
+        return submitGrade(((double)numberCorrect/studentAnswersText.length)*100);
     }
 
-    private void submitGrade(double grade) {
+    private boolean submitGrade(double grade) {
         JSONObject data;
         try {
             JSONObject payload = new JSONObject()
@@ -108,13 +116,16 @@ public class QuizzesActivity extends Activity {
                     .put("payload", payload);
         } catch (JSONException je) {
             je.printStackTrace();
-            return;
+            return false;
         }
 
         new FetchAPIData() {
             @Override
             protected void onFetch(JSONObject data) { }
         }.fetch(data);
+
+        Toast.makeText(getApplicationContext(),"You submitted the quiz.", Toast.LENGTH_LONG).show();
+        return true;
     }
 
     private String[] extractStudentAnswers(ArrayList<EditText> studentAnswers){
@@ -124,4 +135,5 @@ public class QuizzesActivity extends Activity {
         }
         return answers;
     }
-}
+
+ }
