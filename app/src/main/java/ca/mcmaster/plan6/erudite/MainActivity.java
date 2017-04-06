1347f7c5cb2dcaf379b2ffca66bdbc8bd9e49f1b
puruj <puru123456789@hotmail.com>
2017-04-06 19:25:11 -0400

    Added login, main content,and teacher view grade images

--- app/src/main/java/ca/mcmaster/plan6/erudite/MainActivity.java
+++ app/src/main/java/ca/mcmaster/plan6/erudite/MainActivity.java
@@ -15,6 +15,8 @@ public class MainActivity extends Activity {
                    quizzesButton,
                    gradesButton;
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
